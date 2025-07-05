c1a02a424081061242c3e934b8d4c8d4b3b7733e
Luis Felipe Kunzler <kunzler.luis@gmail.com>
2025-07-05 13:20:37 -0300

    fix: variable declaration

--- LC036_Sudoku/go/lc036.go
+++ LC036_Sudoku/go/lc036.go
@@ -1,7 +1,5 @@
 package lc036
 
-import "fmt"
-
 func isValidSudoku(board [][]byte) bool {
 	rows := make([]map[byte]bool, 9)
 	cols := make([]map[byte]bool, 9)
@@ -15,11 +13,11 @@ func isValidSudoku(board [][]byte) bool {
 
 	for r := 0; r < 9; r++ {
 		for c := 0; c < 9; c++ {
+			val := board[r][c]
 			if val == '.' {
 				continue
 			}
 
-			val := board[r][c]
 			squareIdx := (r/3)*3 + c/3
 
 			if rows[r][val] || cols[c][val] || squares[squareIdx][val] {
